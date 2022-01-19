a825440c5bb09cd62b2c73b4d47465bc012d9f42
Stefan <ostojicstefan701@gmail.com>
2022-01-19 02:19:46 +0100

    working version

--- src/index.ts
+++ src/index.ts
@@ -5,38 +5,21 @@ let buttons: any = [];
 document.addEventListener('DOMContentLoaded', () => {
   initBoard();
   getButtonReferences();
-  initBombs(10);
+  initBombs(20);
   board.addEventListener('click', handleButtonClick);
 });
 
-function handleButtonClick(e: Event) {
-  const target = e.target as HTMLElement;
-  if (!target.classList.contains('block')) return;
-
-  if (checkIfTargetIsBomb(target)) {
-    handleGameOver();
-    return;
-  }
-
-  const surroundingBlocks = findCorrespondingBlocks(
-    getSurroundingBlocks(target)
-  );
-
-  const bombNumber = getNumberOfSurroundingBombs(surroundingBlocks);
-  target.textContent = String(bombNumber);
+function getButtonReferences() {
+  buttons = Array.from(board.querySelectorAll('.block'));
 }
 
 function initBoard(): void {
   let output = '';
   for (let i = 0; i < 100; ++i)
     output += `
-        <div data-id=${i} class="block"></div>
-      `;
+          <div data-id=${i} class="block"></div>
+        `;
   board.innerHTML = output;
-}
-
-function getButtonReferences() {
-  buttons = Array.from(board.querySelectorAll('.block'));
 }
 
 function initBombs(n: number): void {
@@ -47,25 +30,124 @@ function initBombs(n: number): void {
   }
 }
 
-function getRandomInt(min: number, max: number) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+function handleButtonClick(e: Event) {
+  const target = e.target as HTMLElement;
+  if (!target.classList.contains('block')) return;
+  if (checkIfTargetIsBomb(target)) {
+    handleGameOver();
+    return;
+  }
+  checkSurroundingBlocks(target);
+}
+
+function checkSurroundingBlocks(block: any) {
+  const surroundingBlocks = getSurroundingBlocks(block);
+  block.classList.add('marked');
+
+  console.log(surroundingBlocks);
+
+  const numBombs = countBombs(surroundingBlocks);
+  console.log(numBombs);
+
+  if (numBombs > 0) {
+    block.classList.add('bomb-near-by');
+    block.textContent = String(numBombs);
+    return;
+  }
+
+  block.classList.add('no-bomb');
+  Array.from(surroundingBlocks).forEach((bl: any) => {
+    checkSurroundingBlocks(bl);
+  });
+}
+
+function checkIfTargetIsBomb(target: any): boolean {
+  // if (!target.dataset.bomb) {
+  //   target.classList.add('marked');
+  // }
+  if (target.dataset.bomb) return true;
+  return false;
 }
 
 function getSurroundingBlocks(block: any): any {
   const blockCoord = parseInt(block.dataset.id);
-  const coords = [];
+  const coords: number[] = [];
   let offset = -10;
 
-  for (let i = 0; i < 3; ++i) {
-    for (let j = -1; j < 2; ++j) {
-      const coord: number = blockCoord + offset + j;
-      coords.push(coord);
+  if (endsWith(blockCoord, 0)) {
+    checkValidCoord(coords, blockCoord - 10);
+    checkValidCoord(coords, blockCoord - 10 + 1);
+    checkValidCoord(coords, blockCoord + 1);
+    checkValidCoord(coords, blockCoord + 10 + 1);
+    checkValidCoord(coords, blockCoord + 10);
+  } else if (endsWith(blockCoord, 9)) {
+    checkValidCoord(coords, blockCoord - 10);
+    checkValidCoord(coords, blockCoord - 10 - 1);
+    checkValidCoord(coords, blockCoord - 1);
+    checkValidCoord(coords, blockCoord + 10 - 1);
+    checkValidCoord(coords, blockCoord + 10);
+  } else if (startsWith(blockCoord, 9)) {
+    checkValidCoord(coords, blockCoord - 1);
+    checkValidCoord(coords, blockCoord - 10 - 1);
+    checkValidCoord(coords, blockCoord - 10);
+    checkValidCoord(coords, blockCoord - 10 + 1);
+    checkValidCoord(coords, blockCoord + 1);
+  } else if ((startsWith(blockCoord, 9), endsWith(blockCoord, 0))) {
+    checkValidCoord(coords, blockCoord - 10);
+    checkValidCoord(coords, blockCoord - 10 + 1);
+    checkValidCoord(coords, blockCoord + 1);
+  } else if ((startsWith(blockCoord, 9), endsWith(blockCoord, 9))) {
+    checkValidCoord(coords, blockCoord - 1);
+    checkValidCoord(coords, blockCoord - 10 - 1);
+    checkValidCoord(coords, blockCoord - 10);
+  } else if (endsWith(blockCoord, 9)) {
+    checkValidCoord(coords, blockCoord - 10);
+    checkValidCoord(coords, blockCoord - 10 - 1);
+    checkValidCoord(coords, blockCoord - 1);
+    checkValidCoord(coords, blockCoord + 10 - 1);
+    checkValidCoord(coords, blockCoord + 10);
+  } else {
+    for (let i = 0; i < 3; ++i) {
+      for (let j = -1; j < 2; ++j) {
+        const coord: number = blockCoord + offset + j;
+        if (coord >= 0 && coord <= 99) {
+          if (!Array.from(buttons[coord].classList).includes('marked'))
+            coords.push(coord);
+        }
+      }
+      offset += 10;
     }
-    offset += 10;
   }
-  return coords;
+
+  return findCorrespondingBlocks(coords);
+}
+
+function checkValidCoord(coords: number[], coord: number): void {
+  if (coord >= 0 && coord <= 99) {
+    if (!Array.from(buttons[coord].classList).includes('marked'))
+      coords.push(coord);
+  }
+}
+
+function countBombs(blocks: any): number {
+  let num = 0;
+  Array.from(blocks).forEach((block: any) => {
+    if (checkIfTargetIsBomb(block)) ++num;
+  });
+
+  return num;
+}
+
+function checkIfBlocksAreBombs(blocks: any) {
+  const numBombs = countBombs(blocks);
+  if (numBombs > 0) {
+    return numBombs;
+  }
+
+  for (let block of blocks) {
+    const anotherBlocks = getSurroundingBlocks(block);
+    checkIfBlocksAreBombs(anotherBlocks);
+  }
 }
 
 function findCorrespondingBlocks(coords: any): any {
@@ -77,21 +159,24 @@ function findCorrespondingBlocks(coords: any): any {
   });
   return blocks;
 }
-
-function getNumberOfSurroundingBombs(sBlocks: any): number {
-  let num = 0;
-  sBlocks.forEach((sb: any) => {
-    if (sb.dataset.bomb) ++num;
-  });
-  return num;
-}
-
-function checkIfTargetIsBomb(target: any): boolean {
-  if (target.dataset.bomb) return true;
-  return false;
-}
-
-function handleGameOver() {
+function handleGameOver(): void {
   console.log('GAME OVER');
   board.removeEventListener('click', handleButtonClick);
 }
+function getRandomInt(min: number, max: number) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function endsWith(num1: number, num2: number): boolean {
+  if (num1 % 10 === num2) return true;
+  return false;
+}
+
+function startsWith(num1: number, num2: number): boolean {
+  if (Math.floor(num1 / 10) === num2) return true;
+  return false;
+}
+
+// test(90);
